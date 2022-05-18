09c724c46b69b74ac65e1bee890c986d798bce97
Youngnam Lee <lyn00227@naver.com>
2022-05-18 17:28:44 +0900

    First Completion - Taxi Class

--- src/Bus.java
+++ src/Bus.java
@@ -4,17 +4,20 @@ public class Bus {
      * 포함 정보
      **/
 
+    // 요금
+    private static final int fare = 1200;
+
     // 최대승객수
-    static final int passengerLimit = 50;
+    private static final int passengerLimit = 50;
 
     // 버스 번호
     private int busNumber;
+
     // 주유량
-    private int gasAmount;
+    private int gasAmount = 100;
+
     //현재 속드
     private int speed;
-    // 요금
-    private int fare;
 
     // 현재 승객수
     private int passengerCount;
@@ -22,19 +25,9 @@ public class Bus {
     // 상태 (운행, 차고지 행), enum Type (run:운행, garage: 차고지행, general: 택시 일반)
     private Status status;
 
-    // 생성자,
-
-    public Bus(int busNumber, int gasAmount) {
-
-        this.passengerCount = 0;
-        this.fare = 1250;
-
-        // 버스 객체 생성 시, 번호는 고유값으로 생성되어야 합니다.
+    public Bus(int busNumber) {
+        // 버스 객체 생성 시, 번호는 고유값으로 생성되어야 합니다. UUID
         this.busNumber = busNumber;
-
-        this.gasAmount = gasAmount;
-
-        this.speed = 0;
 
         // 버스 객체 생성 시, 최초 상태는 '운행' 상태가 됩니다.
         this.status = Status.RUN;
@@ -44,18 +37,23 @@ public class Bus {
      * 기능
      **/
 
-    // 운행, 뭐지?
+    // 운행...
 
     // 버스 상태 변경 (객체 생성 시, 운행 상태)
     public void changeStatus(Status newStatus) {
 
         // 주유량이 떨어지거나, 운행을 종료할 때 '차고지행' 상태로 변경
+        if (gasAmount == 0 || newStatus == Status.garage) {
+            this.status = Status.garage;
+            return;
+        }
 
         // 주유량이 10미만일 경우 '주유가 필요하다'는 메시지 출력
-
         if (gasAmount < 10) {
             System.out.println("주유가 필요합니다.");
         }
+
+        this.status = newStatus;
     }
 
     // 승객 탑승
@@ -63,12 +61,18 @@ public class Bus {
     // 탑승 시, 현재 승객수 증가
     public void getIn(int newPassengerCount) {
 
-        // 이런 것들만 보면 단숨에 모를 수도 있으니 메서드로 처리해주면 좋을 것 같음
-        if (passengerCount + newPassengerCount > passengerLimit || Status.RUN != status) {
+        if (Status.RUN != status) {
+            System.out.println("운행 중인 버스가 아닙니다.");
             return;
         }
 
-        passengerCount += newPassengerCount;
+        int addedCount = passengerCount + newPassengerCount;
+
+        if (addedCount > passengerLimit) {
+            return;
+        }
+
+        passengerCount = addedCount;
     }
 
     // 속도 변경
@@ -76,9 +80,15 @@ public class Bus {
 
         if (gasAmount < 10) {
             System.out.println("주유량을 확인해 주세요.");
+        }
+
+        int calculation = speed + addedSpeed;
+
+        if (0 < calculation && calculation <= 100) {
+            speed += addedSpeed;
             return;
         }
 
-        speed += addedSpeed;
+        speed = (calculation <= 0) ? 0 : 100;
     }
 }
--- src/Taxi.java
+++ src/Taxi.java
@@ -4,37 +4,37 @@ public class Taxi {
      * 포함 정보
      */
 
+    // 기본 요금
+    private static final int basicFare = 3800;
+
+    // 거리당 요금
+    private static final int farePerDistance = 100;
+
+    // 기본거리
+    private static final int basicDistance = 2000;
+
     //택시 번호
     private int taxiNumber;
+
     // 주유량
-    private int gasAmount;
+    private int gasAmount = 100;
+
     // 현재속도
     private int speed;
-    // 기본 요금
-    private int basicFare;
-    // 거리당 요금
-    private int farePerDistance;
 
     // 목적지
     private String destination;
-    // 기본거리
-    private int basicDistance;
+
     // 목적지까지 거리
     private int realDistance;
+
     // 상태(운행 중, 일반)
     private Status status;
 
     // 생성자
-    public Taxi(int taxiNumber, int gasAmount, int speed, int basicFare, int farePerDistance, String destination, int basicDistance, int realDistance, Status status) {
+    public Taxi(int taxiNumber) {
+        // 고유값
         this.taxiNumber = taxiNumber;
-        this.gasAmount = gasAmount;
-        this.speed = speed;
-        this.basicFare = basicFare;
-        this.farePerDistance = farePerDistance;
-        this.destination = destination;
-        this.basicDistance = basicDistance;
-        this.realDistance = realDistance;
-        this.status = status;
     }
 
     /**
@@ -48,40 +48,57 @@ public class Taxi {
             return "운행 불가";
         }
 
-        // 조금 더 고민...
-        return "운행 시작!";
+        return "운행 시작~!";
     }
 
     // 승객 탑승
-    public String getIn() {
+    public String getIn(String destination) {
 
         // 일반 상태일 경우에만 탑승 가능
         if (Status.general != status) {
             return "탑승 불가";
         }
 
-        // setter 혹은 승객 탑승 상태 변경 메서드를 이용하면 좋을 것 같음
+        // 운행
         status = Status.RUN;
-        return "승객 탑승";
+
+        // 목적지 설정
+        this.destination = destination;
+
+        // 목적지까지 거리, 네비 검색
+        this.realDistance = searchNavigation();
+
+        return "승객 탑승 완료!";
+    }
+
+    private int searchNavigation() {
+        // this.destination 를 통해 거리 찾음, 로직은 착한 사람만 보임
+        return 10;
     }
 
     // 속도 변경
     public void changeSpeed(int addedSpeed) {
-        speed += addedSpeed;
+
+        int calculation = speed + addedSpeed;
+
+        if (0 < calculation && calculation <= 100) {
+            speed += addedSpeed;
+            return;
+        }
+
+        speed = (calculation <= 0) ? 0 : 100;
     }
     
     // 거리당 요금 추가
-    public int addFare() {
+    private int addFare() {
         if (realDistance > basicDistance) {
             return ((realDistance - basicDistance) * farePerDistance);
         }
-
         return 0;
     }
 
     // 요금 결제
     public String chargeFare() {
-        return "택시 요금 : " + (basicDistance + addFare()) + "원 입니다.";
+        return "택시 요금 : " + (basicFare + addFare()) + "원 입니다.";
     }
-
 }
